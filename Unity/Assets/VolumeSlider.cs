9bfcc86998fc2a74357ce0031eba1d7bd437e094
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-31 12:30:47 +0800

    fix: Jump

---
+++ Unity/Assets/VolumeSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class VolumeSlider : MonoBehaviour
+{
+    private Slider volumeSlider;
+
+
+    void Start()
+    {
+        volumeSlider = gameObject.GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        volumeSlider.value = GameSettings.volume;
+    }
+
+
+
+}
